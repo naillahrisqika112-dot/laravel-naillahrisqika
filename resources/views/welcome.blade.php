d320fe62fa625cb9150e78a2594e7d70d1b919aa
Naillah Risqika <naillahrisqika112@gmail.com>
2026-06-30 10:12:40 +0700

    feat: create product page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
+    <title>Product</title>
 </head>
 
 <body>
 
-<h1>Halaman Login</h1>
+    <h1>Halaman Product</h1>
 
-<form>
-
-<label>Email</label>
-<input type="email">
-
-<br><br>
-
-<label>Password</label>
-<input type="password">
-
-<br><br>
-
-<button>
-Login
-</button>
-
-</form>
+    <p>Fitur product berhasil dibuat</p>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
-<?php
+<<?php
 
 use Illuminate\Support\Facades\Route;
 
+
+// Halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
 
 
+// Fitur Login
 Route::get('/login', function () {
     return view('login');
+});
+
+
+// Fitur Product
+Route::get('/product', function () {
+    return view('product');
 });
